4cd1860c535e54d9509d55b751ada06805e61dba
Bruno Tavares <brunobtavares@outlook.com>
2024-07-29 15:21:36 -0300

    Created randomizer

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RandomizerComponent } from '../randomizer/randomizer.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RandomizerComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  public items: string[] = [];
+  public showRandomizer: boolean = false;
+  public form: FormGroup = new FormGroup({
+    nameList: new FormControl(),
+    minValue: new FormControl(),
+    maxValue: new FormControl(),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const names = localStorage.getItem('names');
+    if (names) this.form.get('nameList')?.setValue(names);
+  }
+
+  public randomizeName() {
+    this.items = [];
+
+    const names = this.form
+      .get('nameList')
+      ?.value.split('\n')
+      .filter((n: String) => n.trim());
+
+    localStorage.setItem('names', this.form.get('nameList')?.value);
+
+    this.items = names;
+    this.showRandomizer = true;
+  }
+
+  public randomizeNumber() {
+    this.items = [];
+
+    const minValue = Number(this.form.get('minValue')?.value);
+    const maxValue = Number(this.form.get('maxValue')?.value);
+
+    if (minValue == 0 && maxValue == 0) return;
+
+    for (let i = minValue; i <= maxValue; i++) {
+      this.items.push(i.toString());
+    }
+
+    this.showRandomizer = true;
+  }
+
+  public onClose() {
+    this.showRandomizer = false;
+    this.items = [];
+  }
+}
---
+++ src/app/randomizer/randomizer.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-randomizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './randomizer.component.html',
+  styleUrl: './randomizer.component.scss',
+})
+export class RandomizerComponent implements OnInit {
+  @Input() items: string[] = [];
+  @Output() onClose: EventEmitter<void> = new EventEmitter();
+
+  public winner: string = '';
+  public winners: string[] = [];
+
+  private loading: boolean = false;
+
+  ngOnInit(): void {
+    this.randomizer();
+  }
+
+  public async randomizer() {
+    const listSize = this.items.length;
+    if (listSize < 1) {
+      this.winner = 'Todos já foram sorteados!';
+      return;
+    }
+
+    this.loading = true;
+    setTimeout(() => this.stopRandomizer(), 3000);
+
+    while (this.loading) {
+      const randomNumber = Math.floor(Math.random() * listSize);
+      this.winner = this.items[randomNumber];
+      await this.sleep(150);
+    }
+
+    const randomNumber = Math.floor(Math.random() * listSize);
+    this.winner = this.items[randomNumber];
+
+    this.winners.push(this.winner);
+
+    console.clear();
+    console.table(this.winners);
+
+    this.items.splice(randomNumber, 1);
+  }
+
+  public stopRandomizer() {
+    this.loading = false;
+  }
+
+  private sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+}
